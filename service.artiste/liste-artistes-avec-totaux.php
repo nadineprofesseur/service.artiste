23e36a660b04d5cc9a7b67573f3f7cc66b1d286b
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-05-01 12:46:02 -0400

    Decompte des demonstrations par artiste

--- service.artiste/liste-artistes-avec-totaux.php
+++ service.artiste/liste-artistes-avec-totaux.php
@@ -3,7 +3,7 @@
 include "connexion.php";
 // ne pas copier if(!empty($basededonnees)) echo "je suis connectee";
 
-$SQL_LISTE_ARTISTES = "SELECT artiste.id, artiste.nom, SUM(demonstration.id) as total_voyages FROM artiste LEFT JOIN demonstration ON demonstration.id_artiste = artiste.id GROUP BY demonstration.id_artiste"; // TODO optimiser
+$SQL_LISTE_ARTISTES = "SELECT artiste.id, artiste.nom, COUNT(demonstration.id) as total_demos FROM artiste LEFT JOIN demonstration ON demonstration.id_artiste = artiste.id GROUP BY demonstration.id_artiste"; // TODO optimiser
 //echo $SQL_LISTE_ARTISTES;
 
 $requeteListeArtistes = $basededonnees->prepare($SQL_LISTE_ARTISTES);
@@ -22,7 +22,7 @@ foreach($listeArtistes as $artiste){
 	<artiste>
 		<id><?php echo $artiste->id?></id>
 		<nom><?php echo $artiste->nom?></nom>
-		<voyages><?php echo $artiste->total_voyages?></voyages>
+		<demonstration><?php echo $artiste->total_demos?></demonstration>
 	</artiste>	
 
 <?php
