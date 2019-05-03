c0a2016c026375b96ca19cfa16c0359e97bad065
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-05-03 09:44:54 -0400

    Modele de demonstration

---
+++ app/src/modele/Demonstration.java
@@ -0,0 +1,48 @@
+package modele;
+
+public class Demonstration {
+
+	protected int id;
+	protected int idArtiste;
+	protected String titre;
+	protected String resume;
+	protected String date;
+	protected String lieu;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getIdArtiste() {
+		return idArtiste;
+	}
+	public void setIdArtiste(int idArtiste) {
+		this.idArtiste = idArtiste;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getResume() {
+		return resume;
+	}
+	public void setResume(String resume) {
+		this.resume = resume;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getLieu() {
+		return lieu;
+	}
+	public void setLieu(String lieu) {
+		this.lieu = lieu;
+	}
+}
