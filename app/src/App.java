a90221307d8af854a13ca1b4baf8ab1118cdd75a
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2019-05-07 21:44:35 -0400

    Fonction ajouter du DAO qui utilise la méthode POST du protocole HTTP pour communiquer avec le service.

--- app/src/App.java
+++ app/src/App.java
@@ -2,17 +2,26 @@ import java.util.List;
 
 import donnee.ArtisteDAO;
 import modele.Artiste;
+import modele.Demonstration;
 import vue.NavigateurDePages;
 
 public class App {
 
 	public static void main(String[] parametres) {
 
-		//ArtisteDAO artisteDAO = new ArtisteDAO();
+		ArtisteDAO artisteDAO = new ArtisteDAO();
 		//List<Artiste> listeArtistes = artisteDAO.listerArtistes();
 		
+		Demonstration demonstration = new Demonstration();
+		demonstration.setIdArtiste(1);
+		demonstration.setTitre("Les poemes de soiree");
+		demonstration.setResume("Un petit resume");
+		demonstration.setDate("aujourd'hui");
+		demonstration.setLieu("Matane");
+		artisteDAO.ajouterDemonstration(demonstration);
+		
 		// https://stackoverflow.com/questions/11273773/javafx-2-1-toolkit-not-initialized
-		NavigateurDePages.launch(NavigateurDePages.class, parametres);
+		//NavigateurDePages.launch(NavigateurDePages.class, parametres);
 
 	}
 
--- app/src/donnee/ArtisteDAO.java
+++ app/src/donnee/ArtisteDAO.java
@@ -1,12 +1,22 @@
 package donnee;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringBufferInputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,6 +29,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import modele.Artiste;
+import modele.Demonstration;
 
 public class ArtisteDAO {
 
@@ -75,6 +86,69 @@ public class ArtisteDAO {
 		return listeArtistes;
 	}
 	
+	private String convertirHashEnUrl(Map<String,String> arguments)
+	{
+		StringJoiner sj = new StringJoiner("&");
+		for(Map.Entry<String,String> entry : arguments.entrySet())
+			try {
+				sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "=" 
+				     + URLEncoder.encode(entry.getValue(), "UTF-8"));
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+		return sj.toString();
+	}
 	
+	// http://localhost/service.artiste/demonstration-ajouter.php?id_artiste=1&titre=bla&resume=blabla&date=unedate&lieu=Matane
+	public void ajouterDemonstration(Demonstration demonstration)
+	{
+		// TODO factoriser ce code de Hashmap dans le modele dans une fonction exporterTableauAssociatif( )
+		//Map<String,String> arguments = new HashMap<>();
+		//arguments.put("titre", demonstration.getTitre());
+		//arguments.put("resume", demonstration.getResume());
+		//arguments.put("lieu", demonstration.getLieu());
+		//arguments.put("date", demonstration.getDate());
+		//arguments.put("id_artiste", demonstration.getArtiste().getId());
+		
+		// TODO generer cette chaine a partir du modele dans une fonction exporterFormatURL( )
+		// Note : utiliser URLEncoder.encode(entry.getKey(), "UTF-8") pour encoder les valeurs
+		String parametres = "";
+		try {
+			parametres = "id_artiste="+URLEncoder.encode(demonstration.getIdArtiste() + "", "UTF-8")
+								+"&titre="+URLEncoder.encode(demonstration.getTitre(), "UTF-8")
+								+"&resume="+URLEncoder.encode(demonstration.getResume(), "UTF-8")
+								+"&date="+URLEncoder.encode(demonstration.getDate(), "UTF-8")
+								+"&lieu="+URLEncoder.encode(demonstration.getLieu(), "UTF-8");
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		} 
+		// pour tester
+		// parametres  = "id_artiste="+demonstration.getIdArtiste()+"&titre=blabla&resume=blabla&date=unedate&lieu=Quebec"; // pour tester	
+		System.out.println(parametres);
+
+		byte[] message       = parametres.getBytes( StandardCharsets.UTF_8 );
+		String requete        = "http://localhost/service.artiste/demonstration-ajouter.php";
+		URL url;
+		try {
+			url = new URL( requete );
+			HttpURLConnection connexion= (HttpURLConnection) url.openConnection();           
+			connexion.setInstanceFollowRedirects( false );
+			connexion.setRequestMethod( "POST" );
+			connexion.setDoOutput(true);
+			
+			connexion.setRequestProperty( "Content-Type", "application/x-www-form-urlencoded"); 
+			connexion.setFixedLengthStreamingMode(message.length);			
+			connexion.setUseCaches( false );
+			
+			connexion.connect();
+			connexion.getOutputStream().write(message);
+			connexion.disconnect();
+		}
+		catch (IOException e) { e.printStackTrace();}
+		
+	}
+// https://stackoverflow.com/questions/4205980/java-sending-http-parameters-via-post-method-easily
+/*
+*/
 	
 }
